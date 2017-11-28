665be3c1a53600bd4f947bb991f01fc74125fa03
Meejin Kim <jeenywau@gmail.com>
2017-11-28 15:26:30 +0900

    Make modal: no content yet...

--- prototype/app.js
+++ prototype/app.js
@@ -286,7 +286,7 @@ function increaseNodenum(n){
 		n = 1;
 	}
 	resource += n;
-	document.getElementById('nodenum').value = resource; 
+	document.getElementById('nodenum').value = resource;
 }
 
 function decreaseNodenum(){
@@ -378,7 +378,7 @@ function addQtip(ele){
 
 cy.contextMenus({
 	menuItems: [
-		
+
 	    {
 	        id: 'accept-suggestion',
 	        content: 'accept suggestion',
@@ -484,4 +484,29 @@ cy.contextMenus({
 	]
 });
 
-//image movement
+//help button
+// Get the modal
+var modal = document.getElementById("helpModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("helpbtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
